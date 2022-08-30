5865b7e5de2c0feb55593a9dfcad7e5f9c73e11f
caracter2 <caracter2@gmail.com>
2022-08-30 22:38:06 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 import predispatch_daily
 
-new_fig = predispatch_daily.create_date_fig(state = 'NSW', keep_all=False)
+st.title('Day-slice NEM Predispatch Predictions')
+
+state_selected = st.selectbox(
+     'State',
+     ('NSW','QLD','VIC','SA','TAS'))
+
+market_selected = st.selectbox('Market',
+                                ('Energy','LOWER5MIN', 'LOWER60SEC', 'LOWER6SEC', 'LOWERREG',
+                                'RAISE5MIN','RAISE60SEC', 'RAISE6SEC', 'RAISEREG')
+                                )
+
+show_future_settled = st.checkbox('Show future settled prices')
+
+date_selection_type = st.radio(
+     "What date would you like to look at?",
+     ('Today', 'Specific date'))
+
+if date_selection_type == 'Specific date':
+    selected_date = st.date_input('specific date')
+else:
+    selected_date = None
+     
+
+new_fig = predispatch_daily.create_date_fig(date = selected_date,
+                                            state = state_selected,
+                                            keep_all=show_future_settled,
+                                            market = market_selected
+                                            )
 st.plotly_chart(new_fig)
+
+st.button("Re-fresh")
